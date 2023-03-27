76eff5f3ec28898931cb37375140742173ec6e57
Clint Grove <clintelyonson@gmail.com>
2023-03-27 18:00:31 +0100

    less than

--- scripts/roleassign.sh
+++ scripts/roleassign.sh
@@ -1,5 +1,5 @@
 #az synapse role assignment list --workspace-name groovywstest --role "Synapse Administrator" --assignee 4fe7fc36-b425-420f-a3f4-5e14e084eb5e
-if [[ $(az synapse role assignment create --workspace-name groovywstest --role "Synapse Administrator" --assignee '4fe7fc36-b425-420f-a3f4-5e14e084eb5e') >0 ]]
+if [[ $(az synapse role assignment create --workspace-name groovywstest --role "Synapse Administrator" --assignee '4fe7fc36-b425-420f-a3f4-5e14e084eb5e') <0 ]]
 then 
  echo "yes" 
 else 
