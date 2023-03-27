2838b3b3042827886566a7a368a531d6b2148087
Clint Grove <clintelyonson@gmail.com>
2023-03-27 16:16:10 +0100

    try script here

---
+++ scripts/roleassign.ps1
@@ -0,0 +1,3 @@
+# az synapse role assignment create --workspace-name groovyws${{parameters.Environment}} --role "Synapse Administrator" --assignee 4fe7fc36-b425-420f-a3f4-5e14e084eb5e'
+
+print "hello"
