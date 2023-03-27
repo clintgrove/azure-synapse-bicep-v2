c2b43d2285b00bbe40bde6f74b2cf1f8a4c3db5b
Clint Grove <clintelyonson@gmail.com>
2023-03-27 17:51:23 +0100

    uy

--- scripts/roleassign.sh
+++ scripts/roleassign.sh
@@ -1,1 +1,2 @@
-if(az synapse role assignment create --workspace-name groovywstest --role "Synapse Administrator" --assignee '4fe7fc36-b425-420f-a3f4-5e14e084eb5e'){"yes"}else{"false no"}
+az synapse role assignment list --workspace-name groovywstest --role "Synapse Administrator" --assignee 4fe7fc36-b425-420f-a3f4-5e14e084eb5e
+#if(az synapse role assignment create --workspace-name groovywstest --role "Synapse Administrator" --assignee '4fe7fc36-b425-420f-a3f4-5e14e084eb5e'){"yes"}else{"false no"}
