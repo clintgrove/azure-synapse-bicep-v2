9cee913b32b0dfe2b167746c64b13c14ccc08efd
Clint Grove <clintelyonson@gmail.com>
2023-03-28 15:29:05 +0100

    right now

--- scripts/roleassignAdmin.sh
+++ scripts/roleassignAdmin.sh
@@ -1,17 +1,15 @@
 #az synapse role assignment list --workspace-name groovywstest --role "Synapse Administrator" --assignee 4fe7fc36-b425-420f-a3f4-5e14e084eb5e
-objectid=$1
-synapseWS=$2
+synapseWS=$1
+objectid=$2
 echo $objectid
 echo $synapseWS
-az synapse role assignment create --workspace-name $synapseWS --role "Synapse Adminstrator" --assignee $objectid
-
-# if [[ $(az synapse role assignment create --workspace-name $synapseWS --role "Synapse Adminstrator" --assignee $objectid) <0 ]]
-# then 
-#  echo "Assignment already exists" 
-# else 
-#  az synapse role assignment create --workspace-name $synapseWS --role "Synapse Administrator" --assignee $objectid
-#  echo "Assignment applied"
-# fi 
+ if [[ $(az synapse role assignment create --workspace-name $synapseWS --role "Synapse Adminstrator" --assignee $objectid) <0 ]]
+then 
+ echo "Assignment already exists" 
+else 
+ az synapse role assignment create --workspace-name $synapseWS --role "Synapse Administrator" --assignee $objectid
+ echo "Assignment applied"
+fi 
 
 #98c07b1c-fc74-4941-85e3-771788c82c15
 #4fe7fc36-b425-420f-a3f4-5e14e084eb5e
