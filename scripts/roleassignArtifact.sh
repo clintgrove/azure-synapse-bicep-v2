90d1caca00ea14387cc781c3cadbde26dc74c08f
Clint Grove <clintelyonson@gmail.com>
2023-03-28 15:04:18 +0100

    fsd

--- scripts/roleassignArtifact.sh
+++ scripts/roleassignArtifact.sh
@@ -1,9 +1,11 @@
 #az synapse role assignment list --workspace-name groovywstest --role "Synapse Administrator" --assignee 4fe7fc36-b425-420f-a3f4-5e14e084eb5e
-if [[ $(az synapse role assignment create --workspace-name groovywstest --role "Synapse Artifact Publisher" --assignee '98c07b1c-fc74-4941-85e3-771788c82c15') <0 ]]
+objectid='98c07b1c-fc74-4941-85e3-771788c82c15'
+synapseWS='groovywstest'
+if [[ $(az synapse role assignment create --workspace-name $synapseWS --role "Synapse Artifact Publisher" --assignee $objectid ) <0 ]]
 then 
  echo "Assignment already exists" 
 else 
- az synapse role assignment create --workspace-name groovywstest --role "Synapse Artifact Publisher" --assignee '98c07b1c-fc74-4941-85e3-771788c82c15'
+ az synapse role assignment create --workspace-name $synapseWS --role "Synapse Artifact Publisher" --assignee $objectid
  echo "Assignment applied"
 fi 
 
