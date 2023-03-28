d5e52daf8476f3d44d4784774ac4e92c70748520
Clint Grove <clintelyonson@gmail.com>
2023-03-28 13:19:21 +0100

    aaadd

--- scripts/roleassign.sh
+++ scripts/roleassign.sh
@@ -1,7 +1,8 @@
 #az synapse role assignment list --workspace-name groovywstest --role "Synapse Administrator" --assignee 4fe7fc36-b425-420f-a3f4-5e14e084eb5e
 if [[ $(az synapse role assignment create --workspace-name groovywstest --role "Synapse Administrator" --assignee '4fe7fc36-b425-420f-a3f4-5e14e084eb5e') <0 ]]
 then 
- echo "yes" 
+ echo "Assignment already exists" 
 else 
  az synapse role assignment create --workspace-name groovywstest --role "Synapse Administrator" --assignee '4fe7fc36-b425-420f-a3f4-5e14e084eb5e'
+ echo "Assignment applied"
 fi 
