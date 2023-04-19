51b6b5eb3902bf1163ac9f42729c6924d8621cc0
Clint Grove <clintelyonson@gmail.com>
2023-04-19 11:44:55 +0100

    bash script clean up

--- scripts/roleassignAdmin.sh
+++ scripts/roleassignAdmin.sh
@@ -1,6 +1,5 @@
-#az synapse role assignment list --workspace-name groovywstest --role "Synapse Administrator" --assignee 4fe7fc36-b425-420f-a3f4-5e14e084eb5e
-synapseWS=$1 #'groovywstest'
-objectid=$2 #'4fe7fc36-b425-420f-a3f4-5e14e084eb5e'
+synapseWS=$1
+objectid=$2 
 echo $synapseWS
 echo $objectid
  if [[ $(az synapse role assignment create --workspace-name $synapseWS --role "Synapse Administrator" --assignee $objectid) <0 ]]
@@ -11,7 +10,3 @@ else
  echo "Assignment applied"
 fi 
 
-#98c07b1c-fc74-4941-85e3-771788c82c15
-#4fe7fc36-b425-420f-a3f4-5e14e084eb5e
-#b3bd2758-369c-4e4c-8bb6-c2ab93db82ad
-#Synapse Administrator
